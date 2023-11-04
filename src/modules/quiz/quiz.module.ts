f97997d7a73c073e97eb1bb65c928a2825847767
Bruno Palhoti <43323404+BrunoPalhoti@users.noreply.github.com>
2023-11-04 19:21:23 -0300

    feat: add the quizes entity

--- src/modules/quiz/quiz.module.ts
+++ src/modules/quiz/quiz.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuizService } from './services/quiz.service';
 import { QuizController } from './controllers/quiz.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Quiz } from './entities/quiz.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Quiz])],
   providers: [QuizService],
   controllers: [QuizController],
 })
