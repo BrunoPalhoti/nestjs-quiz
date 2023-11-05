5eaa7ee9b470f27e8dddcd8e2eb93890224f74c1
Bruno Palhoti <43323404+BrunoPalhoti@users.noreply.github.com>
2023-11-05 15:10:45 -0300

    feat: create a customized query with relation with questions

--- src/modules/quiz/services/quiz.service.ts
+++ src/modules/quiz/services/quiz.service.ts
@@ -16,6 +16,24 @@ export class QuizService {
     return await this.quizRepository.find();
   }
 
+  async getQuizById(id: number): Promise<Quiz> {
+    const quiz = await this.quizRepository
+      .createQueryBuilder('quizes')
+      .innerJoinAndSelect('quizes.questions', 'questions')
+      .where('quizes.id = :id', { id })
+      .select([
+        'quizes.id',
+        'quizes.title',
+        'quizes.description',
+        'quizes.isActive',
+        'questions.id',
+        'questions.question',
+      ])
+      .getOne();
+
+    return quiz;
+  }
+
   async createQuiz(quizData: CreateQuizDto): Promise<Quiz> {
     return await this.quizRepository.save(quizData);
   }
